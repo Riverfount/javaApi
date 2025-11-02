cf836cec1d6d045b2b634e8934f0cb467c2e6c97
Vicente Marçal <vicente.marcal@gmail.com>
2025-11-02 12:36:18 -0300

    Add UserService and update UserResource for user management.

--- src/main/java/com/riverfount/apijava/resources/UserResource.java
+++ src/main/java/com/riverfount/apijava/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.riverfount.apijava.resources;
 
 import com.riverfount.apijava.entities.User;
+import com.riverfount.apijava.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> getAllUser() {
-        User user = new User(1L, "John Due", "john@due.com", "+18800345342",
-                "123test456");
-        return ResponseEntity.ok(user);
+    public ResponseEntity<List<User>> getAllUser() {
+        List<User> users = service.findAll();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        Optional<User> user = service.findById(id);
+        return ResponseEntity.ok(user.get());
     }
 }
---
+++ src/main/java/com/riverfount/apijava/services/UserService.java
@@ -0,0 +1,26 @@
+package com.riverfount.apijava.services;
+
+import com.riverfount.apijava.entities.User;
+import com.riverfount.apijava.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public Optional<User> findById(Long id) {
+        return repository.findById(id);
+    }
+
+
+}
