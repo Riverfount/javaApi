cfd7478f67937229bda0b8774adb59c8767971f1
Vicente Marçal <vicente.marcal@gmail.com>
2025-11-03 21:49:45 -0300

    Add exception handling for user update in UserService.

--- src/main/java/com/riverfount/apijava/services/UserService.java
+++ src/main/java/com/riverfount/apijava/services/UserService.java
@@ -4,6 +4,7 @@ import com.riverfount.apijava.entities.User;
 import com.riverfount.apijava.repositories.UserRepository;
 import com.riverfount.apijava.services.exceptions.DatabaseException;
 import com.riverfount.apijava.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,9 +43,13 @@ public class UserService {
     }
 
     public User  updateUser(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
